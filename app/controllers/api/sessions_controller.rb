ae63f73d7298bad342409205e1b3ec508712cd24
stearagon <me@stevearagon.io>
2016-12-29 23:49:17 -0500

    Change auth token whenever user signs out

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -1,5 +1,6 @@
 module Api
   class SessionsController < Devise::SessionsController
+      skip_before_action :verify_signed_out_user
       respond_to :html, :json
 
       def create
@@ -11,6 +12,24 @@ module Api
                   }
                   render json: data, status: 201 and return
               end
+          end
+      end
+
+      def destroy
+          super do |user|
+              user = User.find_by(email: params['email'])
+              data = {}
+              if user && Devise.secure_compare(user.authentication_token, params['token'])
+                user.authentication_token = nil
+                user.ensure_authentication_token
+                user.save
+                p user.authentication_token
+                data[:signed_out] = true
+              else
+                data[:signed_out] = false
+              end
+
+              render json: data, status: 201 and return
           end
       end
   end
